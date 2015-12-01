16d43796fda44e868a0f721db199d9ec8d74eaea
Marqin <marqin.pl@gmail.com>
2015-12-01 18:28:41 +0100

    upgrade licence notice, because i do not longer use MS code in utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -1,7 +1,7 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
+/*
+ *  Copyright (c) Hubert Jarosz. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
 
 #include <cstdlib>
 #include <set>
