c2adbfbac357440d877a50570432f3a535586254
Hubert Jarosz <marqin.pl@gmail.com>
2016-04-19 19:40:08 +0200

    minor tweak for c# code

--- Managed.cs
+++ Managed.cs
@@ -8,13 +8,14 @@ using System.Runtime.InteropServices;
 
 public class Managed {
 
-unsafe delegate void myDelegate( IntPtr x );
+[UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+unsafe delegate void myDelegate( IntPtr thisptr );
 
-public static unsafe void runIt( IntPtr obj, IntPtr mem_fun ) {
+public static unsafe void runIt( IntPtr thisptr, IntPtr mem_fun ) {
     Console.WriteLine("Here's C# code:");
 
     myDelegate fun = (myDelegate) Marshal.GetDelegateForFunctionPointer( mem_fun, typeof(myDelegate) );
 
-    fun(obj);  // first argument of member functions in C++ is "this", but it's hidden from us :-)
+    fun(thisptr);  // first argument of member functions in C++ is "this", but it's hidden from us :-)
   }
 }
